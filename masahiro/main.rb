f2643453fca15de79af328855d51dfba23fc64f1
humakusatake <you@example.com>
2021-06-04 16:02:43 +0900

    惑星を公転させる

---
+++ masahiro/main.rb
@@ -0,0 +1,29 @@
+require 'dxruby'
+
+require_relative 'player'
+require_relative 'enemy'
+
+font = Font.new(32) # 追加
+player_img = Image.load("image/player.png")
+enemy_img = Image.load("image/enemy.png")
+
+player = Player.new(100, 100, player_img)
+enemies = []
+10.times do
+  enemies << Enemy.new(rand(0..(640 - 32 - 1)), rand((480 - 32 - 1)), enemy_img)
+end
+
+
+Window.loop do
+  player.update
+  player.draw
+
+  Sprite.draw(enemies)
+
+  Sprite.check(player, enemies)
+  Sprite.check(enemies, enemies)
+  enemies.update
+  enemies.draw
+
+
+end
