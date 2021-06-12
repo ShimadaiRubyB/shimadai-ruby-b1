ad56a19ee4866375a4237c46d1cacf9e99ee3121
username(new) <email(new)>
2021-06-12 11:20:33 +0900

    最低限の指針を記載

--- enemy.rb
+++ enemy.rb
@@ -1,21 +1,26 @@
 class Enemy < Sprite
   #initializeで番号を指定
   #番号にアクセサをつける
-  attr_accessor :number # 追加
-  def initialize(x, y, image, number) # 追加
+  attr_accessor :number
+  def initialize(x, y, image, number)
     @number = number
     @font = Font.new(20)
     super
   end
 
-  def draw
+  def draw #敵の描画
     super
     Window.draw_font(self.x + 10, self.y + 6, @number.to_s, @font)
+  end
+
+  def accelerate #敵の移動量決定
+  end
+
+  def update #敵の移動
   end
 
   def hit
     self.vanish
   end
   #hitとshotを適宜変更
-  #enemyの移動メソッドを追加
 end
